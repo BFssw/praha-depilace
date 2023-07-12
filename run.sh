3a90891bf93b59a20949436316f2235a7929b482
Baikal <ozerow.vlad2018@yandex.ru>
2023-07-12 13:28:17 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo -e "135256785\n" | sudo -S docker build -t localhost:32000/depilace-v-praze:latest .
-echo -e "135256785\n" | sudo -S docker push localhost:32000/depilace-v-praze:latest
-echo -e "135256785\n" | sudo -S microk8s kubectl rollout restart -n instalater24 deployment depilace-v-praze
+sudo docker build -t localhost:32000/depilace-v-praze:latest .
+sudo docker push localhost:32000/depilace-v-praze:latest
+sudo microk8s kubectl rollout restart -n instalater24 deployment depilace-v-praze
